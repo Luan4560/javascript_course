2829740878016d708233de31d61c0fa882c23f04
Luan <9001545@riachuelo.com.br>
2022-02-10 19:59:29 -0300

    this regular and arrow functions

--- Behind_the_scenes/script.js
+++ Behind_the_scenes/script.js
@@ -60,18 +60,72 @@
 
 // This keyword
 
-console.log(this);
+// console.log(this);
 
-const calcAge = function(birthyear) {
-  console.log(2037 - birthyear)
-  console.log(this)
+// const calcAge = function(birthyear) {
+//   console.log(2037 - birthyear)
+//   console.log(this)
+// }
+
+// calcAge(1991)
+
+// const calcAgeArrow = (birthyear) => {
+//   console.log(2037 - birthyear)
+//   console.log(this)
+// }
+
+// calcAgeArrow(1992)
+
+// const matilda = {
+//   year: 2017,
+// }
+
+// matilda.calcAge = jonas.calcAge;
+
+// Regular functions & Arrow functions (This)
+
+const luan = {
+  firstName: 'Luan',
+  year: 1991,
+  calcAge: function() {
+    console.log(this);
+    console.log(2037 - this.year)
+
+    // Solution one 
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self)
+    //   console.log(self.year >= 1981 && self.year <= 1996)
+    // };
+
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this)
+      console.log(this.year >= 1981 && this.year <= 1996)
+    };
+   
+    isMillenial()
+  },
+
+  greet: () => console.log(`${this.firstName}`)
 }
 
-calcAge(1991)
+luan.greet()
+luan.calcAge()
 
-const calcAgeArrow = (birthyear) => {
-  console.log(2037 - birthyear)
-  console.log(this)
+
+// Arguments (this keyword)
+const addExpr = function (a, b) {
+  console.log(arguments)
+  return a + b
 }
 
-calcAgeArrow(1992)
+addExpr(2, 5);
+addExpr(2, 5, 8, 12)
+
+const addArrow = (a, b) => {
+  console.log(arguments)
+  return a + b
+}
+
+addArrow(2, 5, 8)
